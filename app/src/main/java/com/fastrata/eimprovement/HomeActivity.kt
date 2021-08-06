52c6ca8663a136e477a6e67c8228be5c5d3b2145
Dian Tri Budianto <dian.budianto@fastratabuana.co.id>
2021-08-07 00:03:00 +0700

    create toolbar layout independent with main layout

--- app/src/main/java/com/fastrata/eimprovement/HomeActivity.kt
+++ app/src/main/java/com/fastrata/eimprovement/HomeActivity.kt
@@ -1,18 +1,53 @@
 package com.fastrata.eimprovement
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.fastrata.eimprovement.databinding.ActivityHomeBinding
+import com.fastrata.eimprovement.databinding.ToolbarBinding
+import com.fastrata.eimprovement.features.settings.ui.SettingsActivity
 import com.fastrata.eimprovement.features.utils.Tools
+import java.util.*
+import com.fastrata.eimprovement.features.utils.DatePickerCustom.Companion.dialogDatePicker
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var toolbarBinding: ToolbarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
 
-        Tools.setSystemBarColor(this, R.color.colorMainEImprovement)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        toolbarBinding = ToolbarBinding.bind(binding.root)
+        setContentView(binding.root)
+
+        initToolbar()
+        initComponent()
+
+        Tools.setSystemBarColor(this, R.color.colorMainEImprovement, this)
         Tools.setSystemBarLight(this)
+    }
+
+    private fun initToolbar() {
+        val toolbar = toolbarBinding.toolbar
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun initComponent() {
+        binding.apply {
+            filterActivityDate.setOnClickListener {
+                dialogDatePicker(
+                    context = this@HomeActivity, fragmentManager = supportFragmentManager,
+                    themeDark = false, minDateIsCurrentDate = true
+                )
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -22,10 +57,13 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
+            android.R.id.home -> {
+                Toast.makeText(applicationContext, "Testing", Toast.LENGTH_LONG).show()
+            }
             R.id.setting_menu -> {
-                /*Intent(this, SettingsActivity::class.java).also {
+                Intent(this, SettingsActivity::class.java).also {
                     startActivity(it)
-                }*/
+                }
             }
         }
         return super.onOptionsItemSelected(item)
