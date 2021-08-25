8166da29a82519bdc65514936e03d88d96bb5ac9
Dian Tri Budianto <dian.budianto@fastratabuana.co.id>
2021-08-25 09:42:17 +0700

    setup timber init to manage logging

--- app/src/main/java/com/fastrata/eimprovement/di/ThisApplication.kt
+++ app/src/main/java/com/fastrata/eimprovement/di/ThisApplication.kt
@@ -3,7 +3,9 @@ package com.fastrata.eimprovement.di
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import com.fastrata.eimprovement.BuildConfig
 import com.orhanobut.hawk.Hawk
+import timber.log.Timber
 
 class ThisApplication : Application() {
     override fun attachBaseContext(base: Context) {
@@ -15,5 +17,19 @@ class ThisApplication : Application() {
         super.onCreate()
 
         Hawk.init(this).build()
+
+        if (BuildConfig.DEBUG) {
+            // https://ayusch.com/timber-for-android/
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String {
+                    return String.format(
+                        "#Class:%s, #Method:%s, #Line:%s ->",
+                        super.createStackElementTag(element),
+                        element.methodName,
+                        element.lineNumber
+                    )
+                }
+            })
+        }
     }
 }
