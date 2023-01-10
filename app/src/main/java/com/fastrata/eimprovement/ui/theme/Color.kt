969438b7d9eb2f59eeb1f2023603a807ca8c2d4c
Dian Tri Budianto <dian.budianto@fastratabuana.co.id>
2023-01-10 16:50:38 +0700

    implement library jetpack compose

---
+++ app/src/main/java/com/fastrata/eimprovement/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.fastrata.eimprovement.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val colorPrimary = Color(0xFF1976D2)
+val colorPrimaryDark = Color(0xFF1565C0)
+val colorPrimaryLight = Color(0xFF1E88E5)
+val colorAccent = Color(0xFFFF4081)
+val colorAccentDark = Color(0xFFF50057)
+val colorAccentLight = Color(0xFFFF80AB)
+val colorMainEImprovement = Color(0xFF002593)
+
+val colorWhiteLight = Color(0xFFFFFFFF)
+val colorButtonBlue = Color(0xFF2198f3)
+val colorButtonYellow = Color(0xFFf0e80a)
